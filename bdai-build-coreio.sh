29d301949848d65f9f747c6bbb8a918c24a2ef9c
Gustavo Goretkin <ggoretkin@theaiinstitute.com>
2023-02-10 14:58:47 -0500

    Allow `curl -ssL https://...` to work in build
    `RUN curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg` was failing as such
    
    ```
    root@6c606197179b:/tmp# curl -vsSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /tmp/blah.gpg
    *   Trying 185.199.110.133...
    * TCP_NODELAY set
    *   Trying 2606:50c0:8001::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8001::154: Cannot assign requested address
    *   Trying 2606:50c0:8003::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8003::154: Cannot assign requested address
    *   Trying 2606:50c0:8002::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8002::154: Cannot assign requested address
    *   Trying 2606:50c0:8000::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8000::154: Cannot assign requested address
    *   Trying 2606:50c0:8001::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8001::154: Cannot assign requested address
    *   Trying 2606:50c0:8003::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8003::154: Cannot assign requested address
    *   Trying 2606:50c0:8002::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8002::154: Cannot assign requested address
    *   Trying 2606:50c0:8000::154...
    * TCP_NODELAY set
    * Immediate connect fail for 2606:50c0:8000::154: Cannot assign requested address
    ^C
    ```

--- bdai-build-coreio.sh
+++ bdai-build-coreio.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-docker build --platform linux/arm64 -t coreio:l4t_r32.7.1_humble -f Dockerfile.ros.humble .
+sudo docker build \
+	--network=host \ # curl https was not working without this
+	--platform linux/arm64 \
+	-t coreio:l4t_r32.7.1_humble \
+	-f Dockerfile.ros.humble \
+	.
