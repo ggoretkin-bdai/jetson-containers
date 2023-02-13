4b959b3819cb9e20662b5236137bd7421aaede8f
Gustavo Goretkin <ggoretkin@theaiinstitute.com>
2023-02-13 16:28:32 -0500

    Make multiline and add network=host

--- bdai-build-coreio.sh
+++ bdai-build-coreio.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-docker build --platform linux/arm64 -t coreio:l4t_r32.7.1_humble -f Dockerfile.ros.humble .
+
+# the `network` setting is necessary for `curl` on https to work in the build
+sudo docker build \
+	--network=host \
+	--platform linux/arm64 \
+	-t coreio:l4t_r32.7.1_humble \
+	-f Dockerfile.ros.humble \
+	.
